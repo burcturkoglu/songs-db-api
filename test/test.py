dec37a4e80e17f8daab91f0df84b32ef0ae5e690
Burc Turkoglu <burcturkoglu@gmail.com>
2019-02-12 00:12:30 +0300

    small change in test

--- test/test.py
+++ test/test.py
@@ -1,16 +1,26 @@
 import json
 import requests
+from requests_toolbelt.multipart import encoder
 
 with open("songs.json", "r") as f:
-    data = json.load(f)
-    for song in data:
-        r = requests.post('http://localhost:5000/songs/', json=song)
+	data = json.load(f)
+	data = json.dumps(data)
+	
+	headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
+	url = 'http://localhost:5000/songs/'
 
-    f.close()
+	r = requests.post(url, data=data, headers=headers)
+	f.close()
 
 with open("songratings.json", "r") as f:
-    data = json.load(f)
-    for rating in data:
-        r = requests.post('http://localhost:5000/songs/rating/', json=rating)
+	data = json.load(f)
+	data = json.dumps(data)
+	
+	headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
+	url = 'http://localhost:5000/songs/rating/'
 
-    f.close()
+	r = requests.post(url, data=data, headers=headers)
+
+	f.close()
+	
+	
